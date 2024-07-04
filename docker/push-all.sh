1ad2e194af59065cc8a7b4f7e3e1e85a8644d4c2
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-07-04 04:28:27 -0500

    fix: docker tags and repository to ghcr

--- docker/push-all.sh
+++ docker/push-all.sh
@@ -12,27 +12,35 @@ docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/facsimilab-base:latest
 
 docker push docker.io/pranavmishra90/facsimilab-base:latest
 docker push ghcr.io/pranavmishra90/facsimilab-base:latest
+docker push ghcr.io/pranavmishra90/facsimilab-base:$facsimilab_version_num
+
+
+
 
 # Main image
 CONTAINER_NAME="pranavmishra90/facsimilab-main:$facsimilab_version_num"
 echo $CONTAINER_NAME
 
-docker tag $CONTAINER_NAME docker.io/pranavmishra90/facsimilab-base:latest
-docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/facsimilab-base:latest
+docker tag $CONTAINER_NAME docker.io/pranavmishra90/facsimilab-main:latest
+docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/facsimilab-main:latest
 
-docker push docker.io/pranavmishra90/facsimilab-base:latest
-docker push ghcr.io/pranavmishra90/facsimilab-base:latest
+docker push docker.io/pranavmishra90/facsimilab-main:latest
+docker push ghcr.io/pranavmishra90/facsimilab-main:latest
+docker push ghcr.io/pranavmishra90/facsimilab-main:$facsimilab_version_num
+
+
+
 
 # Full image
 CONTAINER_NAME="pranavmishra90/facsimilab-full:$facsimilab_version_num"
 echo $CONTAINER_NAME
 
-docker tag $CONTAINER_NAME docker.io/pranavmishra90/facsimilab-base:latest
-docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/facsimilab-base:latest
+docker tag $CONTAINER_NAME docker.io/pranavmishra90/facsimilab-full:latest
+docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/facsimilab-full:latest
 
-docker push docker.io/pranavmishra90/facsimilab-base:latest
-docker push ghcr.io/pranavmishra90/facsimilab-base:latest
-
+docker push docker.io/pranavmishra90/facsimilab-full:latest
+docker push ghcr.io/pranavmishra90/facsimilab-full:latest
+docker push ghcr.io/pranavmishra90/facsimilab-full:$facsimilab_version_num
 
 # Play an alert tone in the terminal to mark completion
 echo -e '\a'
