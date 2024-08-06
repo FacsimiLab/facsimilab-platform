c5b0f2cc4aa4c2c9fe64ed9e40c7da41202d4e1a
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-08-06 18:48:37 -0500

    feat(dependency): Add pipx, preferring native docker builder, cleanup

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -16,15 +16,11 @@ echo "Building the following container:"
 echo "$CONTAINER_NAME"
 
 # Build the docker container
-export DOCKER_BUILDKIT=1 # use docker buildx caching
-export BUILDX_METADATA_PROVENANCE=max
-
-docker build --build-arg IMAGE_VERSION=$facsimilab_version_num --build-arg CACHEBUST=$(date +%s) -t $CONTAINER_NAME .
-# docker buildx build --load --build-arg IMAGE_VERSION=$facsimilab_version_num --build-arg CACHEBUST=1 -t $CONTAINER_NAME --progress=auto --metadata-file ../metadata/base_metadata.json .
+docker build --progress=auto --build-arg IMAGE_VERSION=$facsimilab_version_num --build-arg CACHEBUST=$(date +%s) --metadata-file ../metadata/01-base_metadata.json -t $CONTAINER_NAME .
 
 # Add additional tags
 docker tag $CONTAINER_NAME docker.io/pranavmishra90/$CONTAINER_NAME
-docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/$CONTAINER_NAME
+docker tag $CONTAINER_NAME gitea.mishracloud.com/pranav/$CONTAINER_NAME
 
 # Calculate the total time
 end_time=$(date +%s)
@@ -39,7 +35,7 @@ echo "Completed: $formatted_date"
 echo "Total time taken: $minutes minutes and $seconds seconds"
 echo ""
 echo ""
-echo "FacsimiLab Docker images:"
+echo "FacsimiLab Docker images: $facsimilab_version_num"
 echo ""
 
-docker image ls | grep facsimilab
+docker image ls | grep facsimilab | grep $facsimilab_version_num
