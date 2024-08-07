c926fab718cf718108368a5a190aae73b4f7a9e7
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-08-06 19:55:09 -0500

    feat: Dynamic build and push scripts; conda-lock
    version number is picked up from a text file inside of the /docker dir
    this is then fed into the build-all.sh and push-all.sh files
    
    we are now using a conda-lock file to ensure proper images. This is
    programmatically generated from the environment.yml file inside of
    the full container's directory.

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-# Set the image version number
-facsimilab_version_num="v0.2.4-lock"
+# Choose a version number ---------------------------------------------------------------------------------
+version_file="build_version.txt"
 
+if [ -f "$version_file" ]; then
+	set_version=$(cat "$version_file" | tr -d '[:space:]')
+else
+	echo "Warning: $version_file not found. Using 'dev' as default."
+	set_version="dev"
+fi
+
+echo "--------------------------------------------------------------------------------"
+docker image ls | grep pranavmishra90/facsimilab
+printf "\n\n"
+echo "--------------------------------------------------------------------------------"
+# Prompt the user for the version number
+read -t 30 -p "Enter the facsimilab_version_num [default is '$set_version' (from ./build_version.txt)]: " user_input
+
+# Remove any whitespace from the user input
+user_input=$(echo "$user_input" | tr -d '[:space:]')
+
+# Determine the facsimilab_version_num to use
+if [ -n "$user_input" ]; then
+	facsimilab_version_num="$user_input"
+elif [ -n "$set_version" ]; then
+	facsimilab_version_num="$set_version"
+else
+	echo "Warning: No default version found. Using 'dev' as default."
+	facsimilab_version_num="dev"
+fi
+
+echo "FacsimiLab version: $facsimilab_version_num"
+
+# Write the chosen version number to the version file
+echo "$facsimilab_version_num" >"$version_file"
+
+# ----------------------------------------------------------------------------------------------------------
 facsimilab_username="coder"
 
 # Build containers
@@ -10,34 +43,16 @@ facsimilab_username="coder"
 export facsimilab_version_num
 export facsimilab_username
 
-# # # Base container
-cd base
+# # # # Base container
+# cd base
 
-bash build.sh
+# bash build.sh
 
+# # # Main container
+# cd ../main && bash build.sh
 
+# # Full container
+# cd ../full && bash build.sh
 
-# # Main container
-cd ../main && bash build.sh
-
-
-
-
-# Full container
-cd ../full && bash build.sh
-
-CONTAINER_NAME="facsimilab-full":$facsimilab_version_num
-
-# CONTAINER_NAME="facsimilab-base":$facsimilab_version_num
-# docker tag $CONTAINER_NAME docker.io/pranavmishra90/$CONTAINER_NAME
-# docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/$CONTAINER_NAME
-
-# CONTAINER_NAME="facsimilab-main":$facsimilab_version_num
-# docker tag $CONTAINER_NAME docker.io/pranavmishra90/$CONTAINER_NAME
-# docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/$CONTAINER_NAME
-
-# docker tag $CONTAINER_NAME docker.io/pranavmishra90/$CONTAINER_NAME
-# docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/$CONTAINER_NAME
-
-# Play an alert tone in the terminal to mark completion
-echo -e '\a'
+# # Play an alert tone in the terminal to mark completion
+# echo -e '\a'
--- docker/full/build.sh
+++ docker/full/build.sh
@@ -15,15 +15,20 @@ echo "Building the following container:"
 echo "$CONTAINER_NAME"
 
 # Build the docker container
-# export DOCKER_BUILDKIT=1 # use docker buildx caching
-# export BUILDX_METADATA_PROVENANCE=max
 
-# docker buildx build --progress=plain --load --build-arg IMAGE_VERSION=$facsimilab_version_num --metadata-file ../metadata/full_metadata.json -t $CONTAINER_NAME .
-docker build --no-cache --build-arg IMAGE_VERSION=$facsimilab_version_num --build-arg CONDA_FILE=facsimilab-bm  -t $CONTAINER_NAME .
+export DOCKER_BUILDKIT=1 # use docker buildx caching
+export BUILDX_METADATA_PROVENANCE=max
+export CONDA_FILE="facsimilab-conda-lock.yml" #environment.yml
+export IMAGE_REPO_PREFIX=""
+export CACHEBUST="100"
+
+# docker buildx build --progress=auto --load --build-arg CACHEBUST="$CACHEBUST" --build-arg IMAGE_REPO_PREFIX=$IMAGE_REPO_PREFIX --build-arg IMAGE_VERSION=$facsimilab_version_num --build-arg CONDA_FILE=$CONDA_FILE --metadata-file ../metadata/full_metadata.json -t gitea.mishracloud.com/pranav/$CONTAINER_NAME .
+
+docker build --build-arg IMAGE_REPO_PREFIX=$IMAGE_REPO_PREFIX --build-arg IMAGE_VERSION=$facsimilab_version_num --build-arg CONDA_FILE=$CONDA_FILE --metadata-file ../metadata/03-full_metadata.json -t $CONTAINER_NAME .
 
 # Add additional tags
 docker tag $CONTAINER_NAME docker.io/pranavmishra90/$CONTAINER_NAME
-docker tag $CONTAINER_NAME ghcr.io/pranavmishra90/$CONTAINER_NAME
+docker tag $CONTAINER_NAME gitea.mishracloud.com/pranav/$CONTAINER_NAME
 
 # Calculate the total time
 end_time=$(date +%s)
@@ -38,7 +43,7 @@ echo "Completed: $formatted_date"
 echo "Total time taken: $minutes minutes and $seconds seconds"
 echo ""
 echo ""
-echo "FacsimiLab Docker images:"
+echo "FacsimiLab Docker images: $facsimilab_version_num"
 echo ""
 
-docker image ls | grep facsimilab
+docker image ls | grep facsimilab | grep $facsimilab_version_num
