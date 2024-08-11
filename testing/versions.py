c2956fe098799a69d233133b61e3bbe0ad5f38eb
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-08-10 20:36:52 -0500

    test(dependencies): Add addtional packages to be tested
    Taken from the Prrx1 notebook for hardware and software testing

--- testing/versions.py
+++ testing/versions.py
@@ -1,7 +1,17 @@
+import os
+import sys
+
+import scanpy as sc
+import scvi
+import torch
+
+from scipy.sparse import csr_matrix
+import pandas as pd
+import numpy as np
+
 import jax
 import jaxlib
 import flax
-import torch
 
 
 # Print library versions
@@ -10,7 +20,34 @@ print("JAXlib version:", jaxlib.__version__)
 print("Flax version:", flax.__version__)
 print("PyTorch version:", torch.__version__)
 
-
-# Print CUDA compatibility
-#print("JAXlib CUDA version:", jaxlib.cuda_version)
 print("PyTorch CUDA version:", torch.version.cuda)
+print("scVI version:", scvi.__version__)
+print("Scanpy version:", sc.__version__)
+
+
+# Check if Pytorch has succssfully detected and loaded an Nvidia GPU with CUDA support
+if torch.cuda.is_available():
+
+    print("## Facsimilab: Nvidia CUDA GPU Detected")
+    print(f"GPU Name: {torch.cuda.get_device_name(0)}")
+    print(f"GPU Available: {torch.cuda.is_available()}")
+
+    print("### System Information")
+
+    print(f"- Python version: `{sys.version}` \n - PyTorch version: `{torch.__version__}`\n - CUDNN version: `{torch.backends.cudnn.version()}`\n - Number CUDA Devices: `{torch.cuda.device_count()}`")
+
+    print("### Devices")
+
+    print(f"- Available devices `{torch.cuda.device_count()}`\n - Active CUDA device: `{torch.cuda.current_device()}`")
+
+    print("Python starts numbering from '0'. Therefore, the `Active CUDA device` name/number is expected to be `0` above.")
+
+else:
+    print("## No CUDA GPU Detected")
+    print("This notebook will use the CPU instead of the GPU. Analysis time is expected to be _**significantly longer, but still possible.**_")
+
+    print(f"GPU Available: {torch.cuda.is_available()}")
+
+    print("### System Information")
+
+    print(f"- Python version: `{sys.version}` \n - PyTorch version: `{torch.__version__}`\n - CUDNN version: `{torch.backends.cudnn.version()}`\n - Number CUDA Devices: `{torch.cuda.device_count()}`")
