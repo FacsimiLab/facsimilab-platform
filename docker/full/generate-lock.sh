6fc46507b46742e8ec2fbc99cd5d480af9428007
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-08-12 15:22:47 -0500

    feat: Add additional scverse packages, rmm

--- docker/full/generate-lock.sh
+++ docker/full/generate-lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-conda-lock --kind lock -p linux-64 -f environment.yml --with-cuda 12.4 --lockfile facsimilab-conda-lock.yml
+conda-lock --mamba --kind lock -p linux-64 -f environment.yml --with-cuda 12.4 --lockfile facsimilab-conda-lock.yml --log-level INFO 2>&1 | tee ../log/conda-lock.log
