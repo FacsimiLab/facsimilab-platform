4164ab909a3f32b9a41be57993cbd2045484c766
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-08-22 20:48:58 -0500

    modify actions

--- testing/main_image.py
+++ testing/main_image.py
@@ -1,4 +1,8 @@
 import datalad.api as dl
+import datetime as date
 
+print(f"### Tested on {date.datetime.now().isoformat(timespec='seconds')}")
+print("")
 print(dl.configuration("get", ["user.name", "user.email"]))
-print(dl.wtf())
+print("")
+print(dl.wtf(sensitive=["python"]))
